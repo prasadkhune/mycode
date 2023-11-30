1a9fcc59e7e7cecb78b41041787c2546f86d5cd8
prasadkhune <prasadkhune@gmail.com>
2023-11-30 15:20:35 +0530

    other

---
+++ datatypes.js
@@ -0,0 +1,9 @@
+const profile = {
+name : "Shraddha khapra" ,
+isfollow : true ,
+post : 195 ,
+followers : "569K" ,
+company : "Apna college" ,
+
+};
+console.log(profile);
--- first.js
+++ first.js
@@ -1,3 +1,14 @@
-console.log("prasad");
-
-
+// console.log( 'prasad')
+// console.log( 'khune')
+// console.log( 'akash')
+// console.log( 'kartik')
+// console.log( "jyoti")
+// const na = 50;
+// console.log(na);
+// let name = "prasad"; //declaration
+// console.log(name);
+// let p = "20";
+// console.log(p);
+let a=3;
+a=5;
+console.log("value=".a);
